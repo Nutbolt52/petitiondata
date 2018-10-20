e9a9f66e15a3b5781c27be79e0f9029272c8c7b9
Nutbolt <Nutbolt52@users.noreply.github.com>
2018-10-20 12:39:38 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -156,7 +156,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
