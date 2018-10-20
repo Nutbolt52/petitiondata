363a72216ed349d522237c1bd0f7ed43c7933cc3
Nutbolt <Nutbolt52@users.noreply.github.com>
2018-10-20 12:39:03 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,7 +75,7 @@
             </div>
         </div>
 
-        @if (count($searches) > 0)
+        @if (count((array)$searches) > 0)
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="panel panel-default">
